34773ed1754c807bc61c206c95ce4d0f879d4c2f
sobanjawaid26 <sobanjawaid26@gmail.com>
2020-07-19 15:48:32 +0530

    Static site for - simple tensorflojs + react app

---
+++ precache-manifest.c9afca5551ee3b0e2d8b3299535fcbaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f486359143339a13235025edeced157",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cec4e8d8b955ca5fa022",
+    "url": "./static/css/main.4516af7c.chunk.css"
+  },
+  {
+    "revision": "e00c1269c57948ede218",
+    "url": "./static/js/2.b00abc76.chunk.js"
+  },
+  {
+    "revision": "9f3ece4be77bc30b04966fe84f4cf2e7",
+    "url": "./static/js/2.b00abc76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec4e8d8b955ca5fa022",
+    "url": "./static/js/main.edaf9247.chunk.js"
+  },
+  {
+    "revision": "63566a7510f037aac277",
+    "url": "./static/js/runtime-main.d5b71669.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
